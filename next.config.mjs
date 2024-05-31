42363fafddbce9e2c67e1eabfd6a036b95be830e
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-05-31 10:28:26 +0600

    Add trip singel page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ibb.co",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/components/Card/SingleCard.tsx
+++ src/components/Card/SingleCard.tsx
@@ -1,23 +1,38 @@
 import Image from "next/image";
-import img from "/public/card.jpg";
 import Link from "next/link";
+import img from "/public/card.jpg";
 
 import { FaArrowRightLong } from "react-icons/fa6";
 
 const SingleCard = ({ card }: any) => {
-  console.log("card page", card);
   return (
     <div className="w-[400px] bg-white text-black">
-      <Image className="" src={img} alt="image" />
+      <Image
+        className=""
+        // src={card?.image}
+        src={img}
+        // width={200}
+        // height={200}
+        alt="image"
+      />
       <div className="p-4">
-        <div className="flex justify-between pb-4">
+        <div className="flex justify-between pb-2">
           <Link href="/">
-            <h1 className="font-semibold text-2xl">Tokio Japan</h1>
+            <h1 className="font-semibold text-2xl">{card?.destination}</h1>
           </Link>
           <h1 className="font-semibold text-2xl text-primaryColor">
             ${card?.budget}
           </h1>
         </div>
+        <div className="flex justify-between">
+          <p className="text-base font-medium">
+            Start Date : {card?.startDate}
+          </p>
+          <p className="text-base font-medium">End Date : {card?.endDate}</p>
+        </div>
+        <p className="line-clamp-2 mt-3 mb-6 text-accent text-gray-500">
+          {card?.description}
+        </p>
         <Link href={`/all-trip/${card?.id}`}>
           <button
             className={`text-[#e44d36] border border-[#e44d36] hover:bg-[#e44d36] hover:text-white py-2 px-20 text-lg font-semibold transition-all duration-200 w-full flex items-center justify-center gap-2`}
