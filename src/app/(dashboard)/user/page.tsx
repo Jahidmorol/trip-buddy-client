9cadf27381238aedc84b5fd4de38f84a6a813ed8
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-06-03 13:06:41 +0600

    change trip post design

--- src/app/(dashboard)/user/page.tsx
+++ src/app/(dashboard)/user/page.tsx
@@ -1,169 +1,276 @@
 "use client";
 
-import React from "react";
+import { useForm, FieldValues, SubmitErrorHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import * as z from "zod";
 
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+const schema = z.object({
+  title: z.string().min(1, { message: "Trip Title is required" }),
+  destination: z.string().min(1, { message: "Trip Destination is required" }),
+  startDate: z.string().min(1, { message: "Trip Start Date is required" }),
+  endDate: z.string().min(1, { message: "Trip End Date is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  tripType: z.string().min(1, { message: "Trip Type is required" }),
+  budget: z
+    .string()
 
-// Define your Zod schema
-const formSchema = z.object({
-  destination: z.string().min(2, {
-    message: "Travel Destination is required!.",
-  }),
-  title: z.string().min(2, {
-    message: "Travel title is required!.",
-  }),
-  location: z.string().min(2, {
-    message: "Travel Location is required!.",
-  }),
-  password: z.string().min(2, {
-    message: "Password is required!.",
-  }),
-  startDate: z
+    .min(1, { message: "Budget is required" }),
+  activities: z
     .string()
-    .min(1, {
-      message: "Start Date is required!.",
-    })
-    .refine((val) => !isNaN(Date.parse(val)), {
-      message: "Invalid date format",
-    }),
-  endDate: z
-    .string()
-    .min(1, {
-      message: "End Date is required!.",
-    })
-    .refine((val) => !isNaN(Date.parse(val)), {
-      message: "Invalid date format",
-    }),
+    .min(1, { message: "Activities are required" })
+    .transform((str) => str.split(",").map((activity) => activity.trim())),
+  image: z
+    .instanceof(FileList)
+    .refine((files) => files.length > 0, { message: "Image is required" }),
 });
 
-// Initialize the default date variable
-const currentDateString = new Date().toISOString().split("T")[0]; // Format: YYYY-MM-DD
-
 const UserPage = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      destination: "",
-      title: "",
-      location: "",
-      password: "",
-      startDate: currentDateString,
-      endDate: currentDateString,
-    },
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
   });
 
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log("Form Values:", values);
-    // Reset the form after successful submission
-    form.reset();
+  const onSubmit: SubmitErrorHandler<FieldValues> = async (data) => {
+    console.log(data);
+    reset();
+
+    // Submit the form data to your API
   };
+
+  const tripTypes = [
+    {
+      label: "Adventure",
+      value: "adventure",
+    },
+    {
+      label: "Beach vacation",
+      value: "beachvacation",
+    },
+    {
+      label: "Cultural exploration",
+      value: "culturalexploration",
+    },
+    {
+      label: "Backpacking",
+      value: "backpacking",
+    },
+    {
+      label: "Road trip",
+      value: "roadtrip",
+    },
+    {
+      label: "Safari",
+      value: "safari",
+    },
+    {
+      label: "Ski trip",
+      value: "skitrip",
+    },
+    {
+      label: "City break",
+      value: "citybreak",
+    },
+    {
+      label: "Cruise",
+      value: "cruise",
+    },
+    {
+      label: "Family vacation",
+      value: "familyvacation",
+    },
+    {
+      label: "Hiking expedition",
+      value: "hikingexpedition",
+    },
+    {
+      label: "Luxury getaway",
+      value: "luxurygetaway",
+    },
+    {
+      label: "Solo travel",
+      value: "solotravel",
+    },
+    {
+      label: "Volunteering trip",
+      value: "volunteeringtrip",
+    },
+    {
+      label: "Wellness retreat",
+      value: "wellnessretreat",
+    },
+  ];
 
   return (
     <div className="pt-4 w-full">
       <h1 className="text-3xl font-semibold pb-4">Post Travel</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="flex flex-col md:flex-row justify-between items-center gap-4">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem className="w-full md:w-2/4">
-                  <FormLabel>Travel Title</FormLabel>
-                  <input
-                    type="text"
-                    autoComplete="off"
-                    placeholder="Enter travel title"
-                    {...field}
-                    className="mt-1 block w-full rounded-xl border-2 border-gray-400 bg-gray-100 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm"
-                    style={{ color: "black" }}
-                  />
-                  <FormMessage className="text-red-600" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="destination"
-              render={({ field }) => (
-                <FormItem className="w-full md:w-2/4">
-                  <FormLabel>Travel Destination</FormLabel>
-                  <input
-                    type="text"
-                    autoComplete="off"
-                    placeholder="Enter travel destination"
-                    {...field}
-                    className="mt-1 block w-full rounded-xl border-2 border-gray-400 bg-gray-100 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm"
-                    style={{ color: "black" }}
-                  />
-                  <FormMessage className="text-red-600" />
-                </FormItem>
-              )}
+      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full">
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="title" className="">
+              Trip Title
+            </label>
+            <input
+              className={`${
+                errors?.title ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="text"
+              {...register("title")}
+              placeholder={
+                errors?.title
+                  ? (errors.title.message as string)
+                  : "Enter Your Trip Title..."
+              }
+              id="title"
             />
           </div>
-          <div className="flex mt-4 flex-col md:flex-row justify-between items-center gap-4">
-            <FormField
-              control={form.control}
-              name="startDate"
-              render={({ field }) => (
-                <FormItem className="w-full md:w-2/4">
-                  <FormLabel>Travel Start Date</FormLabel>
-                  <input
-                    type="date"
-                    autoComplete="off"
-                    placeholder="Enter travel start date"
-                    {...field}
-                    className="mt-1 block w-full rounded-xl border-2 border-gray-400 bg-gray-100 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm"
-                    style={{ color: "black" }}
-                    value={field.value || currentDateString} // Ensure value is a string
-                    onChange={(e) => field.onChange(e.target.value)}
-                  />
-                  <FormMessage className="text-red-600" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="endDate"
-              render={({ field }) => (
-                <FormItem className="w-full md:w-2/4">
-                  <FormLabel>Travel End Date</FormLabel>
-                  <input
-                    type="date"
-                    autoComplete="off"
-                    placeholder="Enter travel end date"
-                    {...field}
-                    className="mt-1 block w-full rounded-xl border-2 border-gray-400 bg-gray-100 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm"
-                    style={{ color: "black" }}
-                    value={field.value || currentDateString} // Ensure value is a string
-                    onChange={(e) => field.onChange(e.target.value)}
-                  />
-                  <FormMessage className="text-red-600" />
-                </FormItem>
-              )}
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="destination">Trip Destination</label>
+            <input
+              className={`${
+                errors?.destination ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="text"
+              {...register("destination")}
+              placeholder={
+                errors?.destination
+                  ? (errors.destination.message as string)
+                  : "Enter Your Trip destination..."
+              }
+              id="destination"
             />
           </div>
+        </div>
+        <div className="flex flex-col md:flex-row items-center gap-4 mt-6 w-full">
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="startDate">Trip Start Date</label>
+            <input
+              className={`${
+                errors?.startDate ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="date"
+              {...register("startDate")}
+              placeholder={
+                errors?.startDate
+                  ? (errors.startDate.message as string)
+                  : "Enter Your Trip startDate..."
+              }
+              id="startDate"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="endDate">Trip End Date</label>
+            <input
+              className={`${
+                errors?.endDate ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="date"
+              {...register("endDate")}
+              placeholder={
+                errors?.endDate
+                  ? (errors.endDate.message as string)
+                  : "Enter Your Trip endDate..."
+              }
+              id="endDate"
+            />
+          </div>
+        </div>
 
-          <div className="border-[3px] border-black mt-6 mx-20 rounded-full" />
-          <Button
-            className="w-full text-white my-6 font-semibold border border-[#e44d36] hover:border-black py-5 text-lg"
-            variant={"outline"}
-            type="submit"
-          >
-            Sign In
-          </Button>
-        </form>
-      </Form>
+        <div className="flex flex-col md:flex-row items-center gap-4 mt-6 w-full">
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="tripType">Trip Type</label>
+            <select
+              id="tripType"
+              className="block w-full rounded-xl text-black border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm"
+              {...register("tripType")}
+            >
+              <option value="">Select A Trip Type</option>
+              {tripTypes.map((type, index) => (
+                <option key={index} value={type?.value}>
+                  {type?.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="budget">Trip Budget</label>
+            <input
+              className={`${
+                errors?.budget ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="number"
+              {...register("budget")}
+              placeholder={
+                errors?.budget
+                  ? (errors.budget.message as string)
+                  : "Enter Your Trip budget..."
+              }
+              id="budget"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row items-center gap-4 mt-6 w-full">
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="activities">Trip Activities</label>
+            <input
+              className={`${
+                errors?.activities ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="text"
+              autoComplete="off"
+              {...register("activities")}
+              placeholder={
+                errors?.activities
+                  ? (errors.activities.message as string)
+                  : "Enter Your Trip activities..."
+              }
+              id="activities"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-full">
+            <label htmlFor="image">Upload Trip Image </label>
+            <input
+              className={`${
+                errors?.image ? "placeholder:text-primaryColor" : ""
+              } block w-full rounded-xl text-white font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+              type="file"
+              {...register("image")}
+              placeholder={
+                errors?.image
+                  ? (errors.image.message as string)
+                  : "Enter Your Trip image..."
+              }
+              id="image"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-1 mt-4">
+          <label htmlFor="description">Description</label>
+          <textarea
+            className={`${
+              errors?.description ? "placeholder:text-primaryColor" : ""
+            } block w-full rounded-xl text-black font-semibold border-2 border-gray-400 p-3 shadow-sm placeholder:font-semibold placeholder:text-black/60 focus:border-[#e44d36] focus:outline-none focus:!text-primaryColor focus:font-semibold focus:ring-1 focus:ring-[#e44d36] sm:text-sm focus:placeholder:text-primaryColor`}
+            {...register("description")}
+            rows={4}
+            placeholder={
+              errors?.description
+                ? (errors.description.message as string)
+                : "Enter Trip Description..."
+            }
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-[#E8604C] text-white hover:bg-black border border-black hover:border-white transition-all font-semibold px-12 mt-8 text-lg py-2 rounded mb-16"
+        >
+          Save changes
+        </button>
+      </form>
     </div>
   );
 };
